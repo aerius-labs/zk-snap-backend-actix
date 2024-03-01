01c1c6da9b6c7034282b38463937c41facc4809c
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2024-03-01 14:16:22 +0530

    feat: add db client in main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 use actix_web::{web, App, HttpServer};
+use app::services::dao_service::{self, DaoService};
 use crate::app::config::init_mongo;
+use crate::app::entities::dao_entity::Dao;
 
 pub mod app;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
-    let db_client = web::Data::new(init_mongo().await);
+    // let db_client = web::Data::new(init_mongo().await);
+    let client = match init_mongo().await {
+        Ok(client) => client,
+        Err(e) => panic!("Failed to connect to MongoDB: {}", e),
+    };
+    let database = client.database("rust");
+    let collection = database.collection::<Dao>("dao");
+
+    let dao_service = DaoService::new(collection);
+    let dao_service_data = web::Data::new(dao_service);
 
     HttpServer::new(move || {
         App::new()
-            .app_data(db_client.clone())
+            .app_data(dao_service_data.clone())
             .configure(app::init::initialize)
     })
     .bind("127.0.0.1:8080")?
