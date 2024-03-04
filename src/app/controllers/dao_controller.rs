bbfa9e7d785635fe86f9e111e0afdff3396189fe
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2024-03-04 12:14:28 +0530

    chore: add status code and add dao service

--- src/app/controllers/dao_controller.rs
+++ src/app/controllers/dao_controller.rs
@@ -2,7 +2,8 @@ use actix_web::{get, post, web, Responder};
 use mongodb::bson::oid::ObjectId;
 use crate::app::{dtos::dao_dto::CreateDaoDto, entities::dao_entity::Dao};
 use serde_json::json;
-use crate::app::services::dao_service::DaoService;
+use crate::app::repository::repository::Repository;
+use crate::app::services::dao_service::create_dao;
 
 #[get("dao/")]
 async fn hello() -> impl Responder {
@@ -13,37 +14,25 @@ async fn hello() -> impl Responder {
 
 #[post("dao/")]
 async fn create(
-    db: web::Data<DaoService>,
+    db: web::Data<Repository<Dao>>,
     dao: web::Json<CreateDaoDto>,
 ) -> impl Responder {
     let dao = dao.into_inner();
     
-    if dao.members.is_empty() {
-        return web::Json(json!({
-            "message": "Members are required"
-        }));
-    }
-
-    let dao_entity = Dao {
-        id: Some(ObjectId::new()),
-        name: dao.name,
-        description: dao.description,
-        logo: dao.logo,
-        members: dao.members,
-    };
-
-    let result = match db.create(dao_entity).await {
+    let result = match create_dao(db, dao).await {
         Ok(result) => result,
         Err(e) => {
             println!("Failed to create DAO: {}", e);
             return web::Json(json!({
-                "message": "Failed to create DAO"
+                "code": 400,
+                "message": "Failed to create DAO",
+                "Error": e.to_string()
             }));
         }
     };
 
-
     web::Json(json!({
+        "code": 201,
         "message": "Creating DAO",
         "ObjectId": result
     }))
