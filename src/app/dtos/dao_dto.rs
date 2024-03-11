7e2f63b3a82ee5ced73180b95ac8a5855fabe77f
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2024-03-11 11:35:40 +0530

    chore: cargo fmt

--- src/app/dtos/dao_dto.rs
+++ src/app/dtos/dao_dto.rs
@@ -3,10 +3,18 @@ use validator::Validate;
 
 #[derive(Debug, Serialize, Deserialize, Validate)]
 pub struct CreateDaoDto {
-    #[validate(length(min = 3, max = 50, message = "Name must be between 3 and 50 characters"))]
+    #[validate(length(
+        min = 3,
+        max = 50,
+        message = "Name must be between 3 and 50 characters"
+    ))]
     pub name: String,
 
-    #[validate(length(min = 3, max = 200, message = "Description must be between 3 and 200 characters"))]
+    #[validate(length(
+        min = 3,
+        max = 200,
+        message = "Description must be between 3 and 200 characters"
+    ))]
     pub description: String,
 
     #[validate(length(min = 3, message = "Logo URL must be between 3 and 200 characters"))]
