7fd16e8dcbc2f69201f70a2a41c7388c21b555c0
Sunil Jalandhra <jalandhrasunil99@gmail.com>
2024-03-15 14:29:13 +0530

    fix: add function to decrypt pvt key and end of proposal

--- src/app/services/proposal_service.rs
+++ src/app/services/proposal_service.rs
@@ -1,14 +1,15 @@
 use chrono::{DateTime, Utc};
 use dotenv::dotenv;
 use mongodb::bson::oid::ObjectId;
-use serde::{Deserialize, Serialize};
+use reqwest::Client;
 use std::env;
 use std::io::{Error, ErrorKind};
+use tokio::time::{sleep_until, Instant};
 
 use super::dao_service;
 use crate::app::entities::proposal_entity::EncryptedKeys;
 use crate::app::{
-    dtos::proposal_dto::CreateProposalDto,
+    dtos::proposal_dto::{CreateProposalDto, DecryptRequest, DecryptResponse},
     entities::{dao_entity::Dao, proposal_entity::Proposal},
     repository::generic_repository::Repository,
 };
@@ -33,6 +34,7 @@ pub async fn create_proposal(
         Err(e) => return Err(e),
     };
 
+    // this generates the encrypted keys
     let encrypted_keys = generate_encrypted_keys(proposal.end_time).await?;
 
     let proposal = Proposal {
@@ -49,9 +51,87 @@ pub async fn create_proposal(
         id: Some(ObjectId::new()),
     };
 
+    // this schedules the event to handle the end of the proposal
+    let proposal_id = proposal.id.unwrap().to_string();
+    schedule_event(&proposal_id, db.clone(), proposal.end_time).await;
+
     match db.create(proposal).await {
         Ok(result) => Ok(result),
         Err(e) => Err(Error::new(ErrorKind::Other, e.to_string())),
+    }
+}
+
+async fn schedule_event(
+    proposal_id: &str,
+    db: web::Data<Repository<Proposal>>,
+    end_time: DateTime<Utc>,
+) {
+    let now = Utc::now();
+    if end_time > now {
+        let wait_duration = (end_time - now).to_std().unwrap();
+        let sleep_time = Instant::now() + wait_duration;
+        sleep_until(sleep_time).await;
+    }
+
+    match handle_event_end(proposal_id, db).await {
+        Ok(_) => (),
+        Err(e) => println!("Failed to handle event end: {}", e),
+    }
+}
+
+async fn handle_event_end(
+    proposal_id: &str,
+    db: web::Data<Repository<Proposal>>,
+) -> Result<(), Error> {
+    let proposal = db.find_by_id(proposal_id).await.unwrap();
+    let mut proposal = match proposal {
+        Some(proposal) => proposal,
+        None => return Err(Error::new(ErrorKind::NotFound, "Proposal not found")),
+    };
+
+    let encrypted_keys = proposal.encrypted_keys;
+    let mut encrypted_keys = encrypted_keys.clone();
+
+    let pvt_key = decrypt_keys(encrypted_keys.pvt_key).await.unwrap();
+
+    encrypted_keys.pvt_key = pvt_key;
+
+    proposal.encrypted_keys = encrypted_keys;
+
+    match db.update(proposal_id, proposal).await {
+        Ok(_) => Ok(()),
+        Err(e) => Err(Error::new(ErrorKind::Other, e.to_string())),
+    }
+}
+
+async fn decrypt_keys(pvt: String) -> Result<String, Error> {
+    dotenv().ok();
+    let url = env::var("DECRYPTION_URL").expect("URL is not set");
+    let client = Client::new();
+
+    // Create the request body
+    let req_body = DecryptRequest { pvt };
+
+    // Send a POST request
+    let response = client.post(url).json(&req_body).send().await;
+
+    match response {
+        Ok(resp) => {
+            if resp.status().is_success() {
+                // Parse the JSON response
+                let json: DecryptResponse = resp.json().await.unwrap();
+                Ok(json.value)
+            } else {
+                Err(Error::new(
+                    std::io::ErrorKind::Other,
+                    "Failed to decrypt keys",
+                ))
+            }
+        }
+        Err(e) => Err(Error::new(
+            std::io::ErrorKind::Other,
+            format!("Failed to send request: {}", e),
+        )),
     }
 }
 
