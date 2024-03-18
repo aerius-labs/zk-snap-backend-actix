8b7d0be2b304de93df8d17a52d07990ccac49a33
shryasss <shreyas_londhe@outlook.com>
2024-03-18 11:33:09 +0530

    fix: fixed Cargo.toml

--- zk-snap-backend/src/app/utils/generate_witness.rs
+++
@@ -1,9 +0,0 @@
-use std::io::Error;
-
-use crate::app::dtos::witness_dto::BaseProofWitness;
-
-pub fn generate_aggregator_base_proof_witness(
-    witness_dto: BaseProofWitness,
-) -> Result<String, Error> {
-
-}
